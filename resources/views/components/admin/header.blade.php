7ad4e47b39d86fccff0e0f794bc6b43c95d6b0c3
rudi97277 <rudi97277@gmail.com>
2024-06-10 19:26:54 +0700

    feat: [header] add attendance button

--- resources/views/components/admin/header.blade.php
+++ resources/views/components/admin/header.blade.php
@@ -6,5 +6,13 @@
                 <path stroke="currentColor" stroke-linecap="round" stroke-width="2" d="M5 7h14M5 12h14M5 17h14" />
             </svg>
             <h1 class="text-md">{{ AppHelper::today() }}</h1>
+            <a class="flex text-sm items-center gap-2 rounded-md p-1 text-white bg-main"
+                href="{{ route('worker.home') }}"><svg class="w-6 h-6 " aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor"
+                    viewBox="0 0 24 24">
+                    <path fill-rule="evenodd"
+                        d="M18 5.05h1a2 2 0 0 1 2 2v2H3v-2a2 2 0 0 1 2-2h1v-1a1 1 0 1 1 2 0v1h3v-1a1 1 0 1 1 2 0v1h3v-1a1 1 0 1 1 2 0v1Zm-15 6v8a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-8H3ZM11 18a1 1 0 1 0 2 0v-1h1a1 1 0 1 0 0-2h-1v-1a1 1 0 1 0-2 0v1h-1a1 1 0 1 0 0 2h1v1Z"
+                        clip-rule="evenodd" />
+                </svg>Absensi</a>
     </div>
 </header>
